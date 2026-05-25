905111896b1ae1d0eb342679166dfa260549a229
Bellia <ferhcp@gmail.com>
2026-05-25 20:13:43 -0300

    ultimo commit repo final se mejora y se corrigen fallas

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 from pages.login_page import LoginPage
 from utils.constants import BASE_URL, VALID_USER, VALID_PASSWORD
@@ -10,27 +9,22 @@ from utils.helpers import tomar_captura_en_falla, obtener_timestamp_legible
 
 @pytest.fixture(scope="function")
 def driver():
-    
     chrome_options = Options()
-
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--no-sandbox")             # Requerido en Linux
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Evita errores de memoria
-    chrome_options.add_argument("--disable-gpu")            # Estabilidad en headless
+    chrome_options.add_argument("--no-sandbox")             
+    chrome_options.add_argument("--disable-dev-shm-usage")  
+    chrome_options.add_argument("--disable-gpu")            
     chrome_options.add_argument("--disable-extensions")
 
-    
-    service = Service(ChromeDriverManager().install())
-    navegador = webdriver.Chrome(service=service, options=chrome_options)
+    navegador = webdriver.Chrome(options=chrome_options)
 
     
-    navegador.implicitly_wait(10)
+    navegador.implicitly_wait(5) 
 
     print(f"\n🚀 Navegador iniciado — {obtener_timestamp_legible()}")
 
-    yield navegador  # ← El test recibe el driver aquí
+    yield navegador  
 
-    
     print(f"\n🔒 Cerrando navegador — {obtener_timestamp_legible()}")
     navegador.quit()
 
@@ -41,8 +35,7 @@ def driver_sesion(driver):
     login.login(VALID_USER, VALID_PASSWORD)
 
     print(f"✅ Sesión iniciada como '{VALID_USER}'")
-
-    return driver  # Retorna el mismo driver ya autenticado
+    return driver  
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
@@ -50,9 +43,13 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     reporte = outcome.get_result()
 
-    
     if reporte.when == "call" and reporte.failed:
-        driver_fixture = item.funcargs.get("driver") or item.funcargs.get("driver_sesion")
+        
+        driver_fixture = None
+        if "driver" in item.fixturenames:
+            driver_fixture = item.funcargs.get("driver")
+        elif "driver_sesion" in item.fixturenames:
+            driver_fixture = item.funcargs.get("driver_sesion")
 
         if driver_fixture:
             nombre_test = item.name.replace(" ", "_")
